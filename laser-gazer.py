56a98dfada90db86f433abec617fe425a680ef5b
Aviv Hurvitz <aviv.hurvitz@gmail.com>
2021-09-15 21:24:03 +0300

    PID controller

--- laser-gazer.py
+++ laser-gazer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from gpiozero import LED, PWMLED
 import sys
 from picamera.array import PiRGBArray
@@ -6,9 +7,12 @@ import time
 import cv2
 import numpy as np
 import copy
+#from pid_jataruku import PID
+from PID import PID
 
-mot_l_pins = [PWMLED(19), PWMLED(26)] # [backward pin, forward pin]
-mot_r_pins = [PWMLED(21), PWMLED(20)] # [backward pin, forward pin]
+freq = 25000
+mot_l_pins = [PWMLED(19, frequency=freq), PWMLED(26, frequency=freq)] # [backward pin, forward pin]
+mot_r_pins = [PWMLED(21, frequency=freq), PWMLED(20, frequency=freq)] # [backward pin, forward pin]
 
 green_led = LED(4)
 red_led = LED(5)
@@ -40,29 +44,46 @@ def detect_laser(img):
     val = ggg[y, x]
     return (x, y), val
 
-
-
+# Init camera
 camera = PiCamera()
-
 frame_size = (320, 240)
 camera.resolution = frame_size
 camera.framerate = 60
 rawCapture = PiRGBArray(camera, size=frame_size)
 
-start = time.time()
 
 DISPLAY_FRAMES_FREQ = 0 # Display every N frames (0 to disable)
 
 
 FLIP_CAMERA = True
 RECORD = False
-
 MOTORS_ON = True
+LASER_INTENSITY_THRESH = 250
+SPEED = 0.6
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-fontScale = 1
-color = (0, 0, 255)
-thickness = 2
+
+dx = 0
+rotation_out = 0
+
+def set_out(out):
+    rotation_out = out
+
+
+pid = PID(P=0.6, I=0.0, D=0.03, current_time=None)
+
+
+def plot_values(img, x, y, val):
+
+    cv2.drawMarker(img, (int(x), int(y)), color=(0,255,0), markerType=cv2.MARKER_CROSS, thickness=2)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 1
+    color = (0, 0, 255)
+    thickness = 2
+
+    cv2.putText(img, str(val), (100, 100), font, fontScale, 
+            color, thickness, cv2.LINE_AA, False)
+
 
 rec_file = 'vid.raw'
 if RECORD:
@@ -72,10 +93,8 @@ if RECORD:
     # writer = cv2.VideoWriter(rec_file, fourcc, fps, frame_size)
     outf = open(rec_file, 'wb')
 
-LASER_INTENSITY_THRESH = 250
-SPEED = 0.3
 
-
+start = time.time()
 iFrame = 0
 for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
     
@@ -91,35 +110,39 @@ for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=
     (x, y), val = detect_laser(img)
 
     if val > LASER_INTENSITY_THRESH:
+
+
         dx = 2 * float(x) / frame_size[0] - 1.0
 
-        if dx >= 0:
+        pid.update(dx)
+        rotation_out = pid.output
+        print(f'dx={dx:.2f} out = {rotation_out:.2f}')
+
+        s = min(abs(rotation_out), 1.0)
+        if rotation_out < 0:
             # Turn right
             green_led.on()
             red_led.off()
             if MOTORS_ON:
-                move_wheel('r', -1, SPEED)
-                move_wheel('l', 1, SPEED)
+                move_wheel('r', -1, s)
+                move_wheel('l', 1, s)
         else:
             # Turn left
             green_led.off()
             red_led.on()
             if MOTORS_ON:
-                move_wheel('r', 1, SPEED)
-                move_wheel('l', -1, SPEED)
+                move_wheel('r', 1, s)
+                move_wheel('l', -1, s)
 
     else:
         green_led.off()
         red_led.off()
         if MOTORS_ON:
-            move_wheel('r', 0, SPEED)
-            move_wheel('l', 0, SPEED)
+            move_wheel('r', 0, 0)
+            move_wheel('l', 0, 0)
 
     if DISPLAY_FRAMES_FREQ > 0 and iFrame % DISPLAY_FRAMES_FREQ == 0:
-        cv2.drawMarker(img, (int(x), int(y)), color=(0,255,0), markerType=cv2.MARKER_CROSS, thickness=2)
-
-        img = cv2.putText(img, str(val), (100, 100), font, fontScale, 
-                    color, thickness, cv2.LINE_AA, False)
+        plot_values(img, x, y, val)
         cv2.imshow('Image', img)
         key = cv2.waitKey(1) & 0xFF
 
@@ -139,11 +162,12 @@ for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=
 
     W = 100
     
-    xstr = [' '] * W
-    xstr[W//2] = '|'
-    xstr[ int(float(x) / frame_size[0] * W) ] = 'x'
-    sys.stdout.write('\r' + ''.join(xstr))
-    sys.stdout.flush()
+    # xstr = [' '] * W
+    # xstr[W//2] = '|'
+    # xstr[ int(float(x) / frame_size[0] * W) ] = 'x'
+    # sys.stdout.write('\r' + ''.join(xstr))
+    # sys.stdout.flush()
+
 
     if RECORD and iFrame >= 500:
         break
